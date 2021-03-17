677526e4eb1b5ce2f869cd4f9d915dd470056c3b
Rvonkrusenstjerna <70213477+Rvonkrusenstjerna@users.noreply.github.com>
2021-03-17 19:38:44 +0100

    changes

--- Source/TrainConsole/Program.cs
+++ Source/TrainConsole/Program.cs
@@ -11,28 +11,34 @@ namespace TrainConsole
         static void Main(string[] args)
         {
             
-            PassengerReader pr = new PassengerReader();
-            List<Object> passengers = pr.Load(@"C:\Users\Robert\Source\Repos\the-train-track-group-11\Source\TrainEngine\FileReaders\PassengerReader.cs");
+            //PassengerReader pr = new PassengerReader();
+            //List<Object> passengers = pr.Load(@"C:\Users\Robert\Source\Repos\the-train-track-group-11\Data\passengers.txt");
 
             StationReader sr = new StationReader();
-            List<Object> stations = sr.Load(@"C:\Users\Robert\Source\Repos\the-train-track-group-11\Source\TrainEngine\FileReaders\PassengerReader.cs");
+            List<Object> stations = sr.Load(@"C:\Users\Robert\Source\Repos\the-train-track-group-11\Data\stations.txt");
 
             TimetableReader tr = new TimetableReader();
-            List<Object> timetable = pr.Load(@"C:\Users\Robert\Source\Repos\the-train-track-group-11\Source\TrainEngine\FileReaders\PassengerReader.cs");
+            List<Object> timetable = tr.Load(@"C:\Users\Robert\Source\Repos\the-train-track-group-11\Data\timetable.txt");
 
-            TrainReader trr = new TrainReader();
-            List<Object> trains = pr.Load(@"C:\Users\Robert\Source\Repos\the-train-track-group-11\Source\TrainEngine\FileReaders\PassengerReader.cs");
+            //TrainReader trr = new TrainReader();
+            //List<Object> trains = pr.Load(@"C:\Users\Robert\Source\Repos\the-train-track-group-11\Source\TrainEngine\FileReaders\PassengerReader.cs");
 
-            foreach (Passenger x in passengers)
+            //foreach (Passenger x in passengers) Works
+            //{
+            //    Console.WriteLine(x.Name);
+            //}
+
+            foreach(TimeTableEntry x in timetable)
             {
-                Console.WriteLine(x.Name);
+                Console.WriteLine(x.TrainId) ;
             }
 
 
 
-           
 
-           
+
+
+
         }
     }
 }
--- Source/TrainEngine/FileReaders/TimetableReader.cs
+++ Source/TrainEngine/FileReaders/TimetableReader.cs
@@ -12,17 +12,23 @@ namespace TrainEngine.FileReaders
         public List<object> Load(string url)
         {
             
+
+
             string inputData = new StreamReader(
                             File.Open(url, FileMode.Open)
                                             ).ReadToEnd();
 
             string[] dataArray = inputData.Split("\n");
             var ListOfTime = new List<object>();
-            foreach (string entry in dataArray)
+
+            for (int i = 1; i < dataArray.Length; i++)
             {
-                string[] StationData = entry.Split(",");
-                ListOfTime.Add(new TimeTableEntry(Convert.ToInt32(entry[0]), Convert.ToInt32(entry[1]), Convert.ToDateTime(entry[2]), Convert.ToDateTime(entry[3])));
+                
+                string[] StationData = dataArray[i].Split(",");
+                ListOfTime.Add(new TimeTableEntry(Convert.ToInt32(StationData[0]), Convert.ToInt32(StationData[1]), Convert.ToDateTime(StationData[2]), Convert.ToDateTime(StationData[3])));
             }
+            
+            
             return ListOfTime;
         }
     }
